d6401a3e985d4dc9b82453b07322be648ef1250c
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2023-11-03 18:54:46 +0100

    added CORS

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import torch
 from model import ClassificationModel  # Import your model definition
 from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the pre-trained model
 input_dim = 8
