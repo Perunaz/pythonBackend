b24896c953e1760a14d15779778b75eedbd93c93
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2023-11-04 23:02:48 +0100

    Update server.py

--- server.py
+++ server.py
@@ -62,4 +62,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, ssl_context=('/etc/ssl/certs/selfsigned.crt', '/etc/ssl/private/selfsigned.key'))
